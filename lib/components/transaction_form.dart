94ec86973a1eed78350cc30f5415af37fd376ac3
ermessias <ermessias@uolinc.com>
2021-11-10 18:58:25 -0300

    commit init

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,111 @@
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionFrom extends StatefulWidget {
+  final void Function(String title, double value, DateTime date) onSubmit;
+
+ TransactionFrom(this.onSubmit);
+
+  @override
+  _TransactionFromState createState() => _TransactionFromState();
+}
+
+class _TransactionFromState extends State<TransactionFrom> {
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+   DateTime _selectDate = DateTime.now();
+
+_submitForm(){
+
+   final title = _titleController.text;
+   final value = double.tryParse(_valueController.text)?? 0.0;
+
+  if(title.isEmpty || value <= 0.0){
+    return;
+  }
+
+   widget.onSubmit(title, value, _selectDate);
+}
+
+
+
+_showDatePicker(){
+  showDatePicker(context: context,
+  initialDate: DateTime.now(),
+  firstDate: DateTime(2019),
+  lastDate: DateTime.now(),
+).then((datePiker) {
+    if(datePiker == null){
+      return;
+    }
+
+      setState(() {
+         _selectDate = datePiker;
+      });
+   
+});
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return  Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                       onSubmitted: (_) => _submitForm(),
+                      decoration: InputDecoration(
+                        labelText: 'Titulo'
+                      ),
+                    ),
+                    TextField(
+                      controller: _valueController,
+                      keyboardType: TextInputType.numberWithOptions(decimal: true),
+                      onSubmitted: (_) => _submitForm(),
+                      decoration: InputDecoration(
+                        labelText: 'valor'
+                      ),
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text( _selectDate == null ? 'Nenhuma data selecionada!' :
+                                                       'Data selecionada: ${DateFormat('d/M/y').format(_selectDate)}' 
+                            ),
+                          ),
+                          FlatButton(
+                            onPressed: _showDatePicker, 
+                            child: Text('Selecionar Data',
+                             style: TextStyle(
+                               fontWeight: FontWeight.bold,
+                               ),
+                            ),
+                             textColor: Theme.of(context).primaryColor
+                             ,)
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        RaisedButton(onPressed: _submitForm
+                        , 
+                        child: Text('Nova transação'),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white ,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+
+              ),
+            );
+  }
+}
---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,77 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+
+
+final List<Transaction> transactions;
+final void Function(String) removeTransaction;
+
+TransactionList(this.transactions, this.removeTransaction);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: transactions.isEmpty ? Column(
+        children: [
+          SizedBox(height:20),
+          Text('Nenhuma Transação Cadastrada!',
+          style: Theme.of(context).textTheme.headline6
+          ),
+          SizedBox(height:20),
+          Container(
+            height: 200,
+            child: Image.asset('assets/images/waiting.png',
+            fit: BoxFit.cover,
+            ),
+            
+          )
+        ],
+      ) :
+       ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index){
+          final tr = transactions[index];
+          return Card(
+            elevation: 5,
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5,
+            ),
+                child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('R\$${tr.value}'),
+                          ) 
+                      ,),
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date)
+                    ), 
+                    trailing: IconButton(
+                     onPressed: () => removeTransaction(tr.id),
+                     color: Theme.of(context).errorColor,
+                     icon: Icon(Icons.delete)
+                     )
+                      ,
+
+                  ),
+                  
+              );
+        },
+      )
+        
+      );
+  }
+}
